e4375f03cbd5e0a60051b268d63d8d30b812084e
D3rise <derise2000@gmail.com>
2021-01-06 13:36:15 +0300

    first commit :tada:

---
+++ EducationalProject1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EducationalProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> multipleNumbers = new List<int>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                
+            }
+        }
+
+        static bool IsDivisibleBy(int number, int divider)
+        {
+            return number % divider == 0;
+        }
+    }
+}
