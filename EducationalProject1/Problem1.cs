706894d886dc5142462eb0686fbeb95201397128
D3rise <derise2000@gmail.com>
2021-01-06 15:11:13 +0300

    Solution for problem #1

---
+++ EducationalProject1/Problem1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EducationalProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multipleNumbersSum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (IsDivisibleBy(i, 3) || IsDivisibleBy(i, 5))
+                {
+                    multipleNumbersSum += i;
+                }
+            }
+            
+            Console.WriteLine($"Sum of dividable numbers is: {multipleNumbersSum}");
+        }
+
+        static bool IsDivisibleBy(int number, int divider)
+        {
+            return number % divider == 0;
+        }
+    }
+}
